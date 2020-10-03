56c1897e0e20ddc43c51cb2748eb5408e07d3b8c
justdaniel99 <justdaniel99@gmail.com>
2020-10-03 20:15:48 +0300

    Task 1 (ClassLibraryProjects folder with  eponymous solution; ImageRecognitionLibrary library with necessary image recognition function located in Class1.cs; test program (with Main) in ShowCase folder)

---
+++ ClassLibraryProjects/ImageRecognitionLibrary/ShowCase/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using ObjectsImageRecognitionLibrary;
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Type on the keyboard the name of the directory with the images to recognize and press \"Enter\": ");
+            string directory = Console.ReadLine();
+            ObjectsImageRecognitionLibrary.ImageRecognitionLibrary.ProgramStart(directory);
+        }
+    }
