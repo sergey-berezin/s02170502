4e5b45795daacda1e358a2b6829c0b5f21ba3c62
justdaniel99 <justdaniel99@gmail.com>
2020-10-10 22:25:37 +0300

    New version of testing program (with Main)

--- ClassLibraryProjects/ImageRecognitionLibrary/ShowCase/Program.cs
+++ ClassLibraryProjects/ImageRecognitionLibrary/ShowCase/Program.cs
@@ -3,10 +3,21 @@ using ObjectsImageRecognitionLibrary;
 
     class Program
     {
+        // Event for output
+        public static void EventHandler(object sender, ObjectInImageProbability structureObject)
+        {
+            Console.WriteLine($"Looks like it is a {structureObject.ClassLabel} at {structureObject.Path} with probability of {structureObject.Probability}%.");
+        }
+
         static void Main(string[] args)
         {
-            Console.WriteLine("Type on the keyboard the name of the directory with the images to recognize and press \"Enter\": ");
+            // Getting directory from console
+            Console.WriteLine("Type on the keyboard the name of the existing directory (otherwise, the default directory will be used) with the images to recognize and press \"Enter\":");
             string directory = Console.ReadLine();
-            ObjectsImageRecognitionLibrary.ImageRecognitionLibrary.ProgramStart(directory);
+            
+            // Library object creation, subscription to the event and recognition of objects in image
+            ImageRecognitionLibrary LibraryObject = new ImageRecognitionLibrary();
+            LibraryObject.ResultEvent += EventHandler;
+            LibraryObject.ProgramStart(directory);
         }
     }
